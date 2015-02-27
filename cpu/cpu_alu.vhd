15ad8cdc89edfcc1f9c08350a958647a6d92dd84
daniw <dani.winz@bluewin.ch>
2015-02-27 19:01:02 +0100

    Update from svn repository

--- cpu/cpu_alu.vhd
+++ cpu/cpu_alu.vhd
@@ -1,12 +1,14 @@
 -------------------------------------------------------------------------------
 -- Entity: cpu_alu
 -- Author: Waj
--- Date  : 26-May-13
+-- Date  : 28-Feb-14
 -------------------------------------------------------------------------------
--- Description: (ECS Uebung 9)
+-- Description:
 -- ALU for the RISC-CPU of the von-Neuman MCU.
+-- The ALU is purely combinational, and thus no .enb signal in the alu_in
+-- is required.
 -------------------------------------------------------------------------------
--- Total # of FFs: ... tbd ...
+-- Total # of FFs: 0
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -30,19 +32,46 @@ architecture rtl of cpu_alu is
 begin
   
   -----------------------------------------------------------------------------
-  -- ISE workaround 2
-  -----------------------------------------------------------------------------  
-  g_ISE2: if ISE_TOOL generate
-    with ("00" & alu_in.op) select
-    result <= std_logic_vector(unsigned(oper1) + unsigned(oper2))   when OPC(add),
-              std_logic_vector(unsigned(oper1) + unsigned(oper2))   when OPC(add),
-              oper1 and oper2                                       when OPC(andi),
-              oper1 or oper2                                        when OPC(ori),
-              oper1 xor oper2                                       when OPC(xori),
-              oper1(DW-2 downto 0) & "0"                            when OPC(slai),
-              oper1(DW-1) & oper1(DW-1 downto 1)                    when OPC(srai),
-              oper1                                                 when OPC(mov),
-              (others => '0')                                       when others;
-  end generate;
-  
+  -- ISE workaround (:-((
+  -----------------------------------------------------------------------------
+  g_ISE: if ISE_TOOL generate 
+    with to_integer(unsigned(alu_in.op)) select result <=
+      -- Opcode 0: add
+      std_logic_vector(unsigned(oper1) + unsigned(oper2)) when 0,
+      -- Opcode 1: sub
+      std_logic_vector(unsigned(oper1) - unsigned(oper2)) when 1,
+      -- Opcode 2: and
+      oper1 or oper2                                      when 2,
+      -- Opcode 3: or
+      oper1 or oper2                                      when 3,
+      -- Opcode 4: xor
+      oper1 xor oper2                                     when 4,
+      -- Opcode 5: slai
+      oper1(DW-2 downto 0) & '0'                          when 5,
+      -- Opcode 6: srai
+      oper1(DW-1) & oper1(DW-1 downto 1)                  when 6,
+      -- Opcode 7: mov
+      oper1                                               when 7,
+      -- other (ensures memory-less process)
+      (others => '0')                                     when others;
+  end generate g_ISE;
+
+  -----------------------------------------------------------------------------
+  -- More elegant solution using type attribute 'val. Unfortunately, this
+  -- attribute is not supported by ISE XST, but works fine with Vivado.
+  -- (also note that the complementary attribute to 'val is 'pos)
+  -----------------------------------------------------------------------------
+  g_NOT_ISE: if not ISE_TOOL generate 
+    with t_alu_instr'val(to_integer(unsigned(alu_in.op))) select result <=
+      std_logic_vector(unsigned(oper1) + unsigned(oper2)) when add,
+      std_logic_vector(unsigned(oper1) - unsigned(oper2)) when sub,
+      oper1 or oper2                                      when andi,
+      oper1 or oper2                                      when ori,
+      oper1 xor oper2                                     when xori,
+      oper1(DW-2 downto 0) & '0'                          when slai,
+      oper1(DW-1) & oper1(DW-1 downto 1)                  when srai,
+      oper1                                               when mov,
+      (others =>'0')                                      when others;
+  end generate g_NOT_ISE;
+
 end rtl;
