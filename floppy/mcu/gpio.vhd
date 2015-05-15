42837d1b80953363eea519f66260b8bee15333d1
amzflexit <timm.baumann@hotmail.com>
2015-05-15 21:06:40 +0200

    inout via mcu inout program

--- floppy/mcu/gpio.vhd
+++ floppy/mcu/gpio.vhd
@@ -35,7 +35,7 @@ end gpio;
 architecture rtl of gpio is
   
 		signal in_1, in_2 : std_logic_vector(7 downto 0);
-		-- signal out_1 : std_logic_vector(7 downto 0);
+		signal next_out, current_out : std_logic_vector(7 downto 0);
 		
 		
 begin
@@ -49,7 +49,7 @@ begin
 --  to_LED(3) <= from_BTN_ROT_C;
 --  to_LED(2) <= from_BTN_EAST;
 --  to_LED(1) <= from_BTN_WEST;
---  to_LED(0) <= from_BTN_NORTH;
+--  to_LED(7) <= from_BTN_NORTH;
   
   
 
@@ -69,18 +69,32 @@ begin
 		end if;
 	end process;
 	
+	to_LED <= current_out;
+	current_out <= next_out;
+	
+--	P_outsave : process(rst, clk)
+--	begin
+--		if rst = '1' then
+--			current_out <= (others => '0');
+--		elsif rising_edge(clk) then
+--			current_out <= next_out; 
+--		end if;
+--	end process;
+	
   
   P_busaccess : process(rst, clk)
   begin
     if rst = '1' then
       bus_out.data <= (others => '0');
     elsif rising_edge(clk) then
+		next_out <= current_out;
 		bus_out.data(7 downto 0) <= in_2;
 		bus_out.data(15 downto 8) <= (others => '0');
       if bus_in.we = '1' then -- write to register
-        if unsigned(bus_in.addr) = to_unsigned(16#81#,AW) then
-				to_LED <= bus_in.data(7 downto 0);
+        if unsigned(bus_in.addr) = to_unsigned(16#01#,AWL) then
+				next_out <= bus_in.data(7 downto 0);--<= "01010111";
         end if;
+		  
       end if;
     end if;
   end process;
