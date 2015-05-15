a3b7c4df12d6bc2a0327a8601249b6cda192d601
amzflexit <timm.baumann@hotmail.com>
2015-05-15 18:45:13 +0200

    added buttons and removed unused pins

--- floppy/mcu/gpio.vhd
+++ floppy/mcu/gpio.vhd
@@ -20,11 +20,15 @@ entity gpio is
        bus_in  : in  t_bus2rws;
        bus_out : out t_rws2bus;
        -- GPIO pin signals
-       pin_in  : in  t_gpio_pin_in;
-       pin_out : out t_gpio_pin_out;
-		 
+       -- pin_in  : in  t_gpio_pin_in;
+       -- pin_out : out t_gpio_pin_out;
+		 -- LED, Switches and Buttons
 		 to_LED : out std_logic_vector(7 downto 0);
-		 from_SW : in std_logic_vector(3 downto 0)
+		 from_SW : in std_logic_vector(3 downto 0);
+		 from_BTN_ROT_C : in std_logic;
+		 from_BTN_EAST : in std_logic;
+		 from_BTN_WEST : in std_logic;
+		 from_BTN_NORTH : in std_logic
        );
 end gpio;
 
@@ -39,7 +43,10 @@ begin
   -- # of FFs: ......
   -----------------------------------------------------------------------------  
   to_LED(7 downto 4) <= from_SW;
-  to_LED(3 downto 0) <= from_SW;
+  to_LED(3) <= from_BTN_ROT_C;
+  to_LED(2) <= from_BTN_EAST;
+  to_LED(1) <= from_BTN_WEST;
+  to_LED(0) <= from_BTN_NORTH;
   
   P_dummy: process(rst, clk)
   begin
@@ -48,15 +55,15 @@ begin
     elsif rising_edge(clk) then
       if bus_in.we = '1' then
         if unsigned(bus_in.addr) > 0 then
-          bus_out.data <= bus_in.data;
-          pin_out.out_0 <= pin_in.in_0;
-          pin_out.out_1 <= pin_in.in_1;
-          pin_out.out_2 <= pin_in.in_2;
-          pin_out.out_3 <= pin_in.in_3;
-          pin_out.enb_0 <= pin_in.in_3 and pin_in.in_0;
-          pin_out.enb_1 <= pin_in.in_0 and pin_in.in_1;
-          pin_out.enb_2 <= pin_in.in_1 and pin_in.in_2;
-          pin_out.enb_3 <= pin_in.in_2 and pin_in.in_3;
+          -- bus_out.data <= bus_in.data;
+--          pin_out.out_0 <= pin_in.in_0;
+--          pin_out.out_1 <= pin_in.in_1;
+--          pin_out.out_2 <= pin_in.in_2;
+--          pin_out.out_3 <= pin_in.in_3;
+--          pin_out.enb_0 <= pin_in.in_3 and pin_in.in_0;
+--          pin_out.enb_1 <= pin_in.in_0 and pin_in.in_1;
+--          pin_out.enb_2 <= pin_in.in_1 and pin_in.in_2;
+--          pin_out.enb_3 <= pin_in.in_2 and pin_in.in_3;
         end if;
       end if;
     end if;
